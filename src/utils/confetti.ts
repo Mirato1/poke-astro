17f156030bea2418a066eea9cb4892d3b410865e
Lucas Nahuel Giménez <luquetti.5@hotmail.com>
2025-11-16 20:32:20 -0300

    ✨ Add canvas-confetti library and update project structure: remove Welcome component, enhance Layout with dynamic title, and improve global styles for Pokémon cards.

---
+++ src/utils/confetti.ts
@@ -0,0 +1,16 @@
+import confetti from 'canvas-confetti'
+
+export const throwConfetti = () => {
+  confetti({
+    particleCount: 100,
+    spread: 100,
+    angle: 60,
+    origin: { x: 0, y: 1 },
+  })
+  confetti({
+    particleCount: 100,
+    spread: 100,
+    angle: 120,
+    origin: { x: 1, y: 1 },
+  })
+}
