836827b2e365ed5e931cab4705aff68ebb51d3f4
Lucas Nahuel Giménez <luquetti.5@hotmail.com>
2025-11-16 21:48:29 -0300

    ✨ Update components and configuration: integrate API_URLS for sprite images, enhance ThemeManager with rerun attribute, and switch to astro:assets for background image handling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,5 @@ export default defineConfig({
   },
   integrations: [icon()],
   output: 'server',
+  prefetch: false,
 })
---
+++ src/utils/apiUrls.ts
@@ -0,0 +1,5 @@
+const API_URLS = {
+  SPRITES: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/',
+}
+
+export { API_URLS }
